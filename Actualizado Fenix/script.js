4a186c7a85cd95bc2ef397a862e289dbf69f7e75
Bryan Jiménez <66561578+BryanJP19@users.noreply.github.com>
2025-04-11 15:47:35 -0400

    Bryan

--- Actualizado Fenix/script.js
+++ Actualizado Fenix/script.js
@@ -23,7 +23,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 });
 
-// Import the functions you need from the SDKs you need
+/*// Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-analytics.js";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -43,4 +43,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const analytics = getAnalytics(app);*/
